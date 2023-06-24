f8fb4b48ea07cb27a70fd717df9ee338e7bb4547
luck <loicksauvage49@gmail.com>
2023-06-24 17:46:13 +0200

    presque la fin du projet

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,12 @@
-function playGame() {
-    let choice    = wordOrSentence()
-    let score     = 0
-    let totalWord = 0
-    if (choice === "mots")  {
-       score = playBoucleGame(listWords)
-        totalWord = listWords.length
-    } else {
-        score = playBoucleGame(sentence)
-        totalWord = sentence.length
-    }
-
-    displayResult(score, totalWord)
-}
 
 playGame()
+
+
+// let input = document.getElementById("inputEcriture")
+// let button = document.getElementById("btnValiderMot")
+
+// let wordDisplay  = document.querySelector(".zoneProposition")
+// let scoreDisplay = document.querySelector(".zoneScore span") 
+
+// let displayChoice = document.querySelectorAll(".optionsource input")
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,95 @@
 function displayResult(score, totalWord){
-    let resultat = alert("vous avez " + score + " points " + "Sur " + totalWord )
+  let spanScore = document.querySelector(".zoneScore span")
+  let displayyScore = `${score} / ${totalWord}`
+  spanScore.innerText = displayyScore
 }
 
-function wordOrSentence() {
-    let choice = prompt ("Veuillez choisir la liste : mots ou phrases")
-while(choice !== "mots" && choice !== "phrases") {
-    choice = prompt ("Veuillez choisir la liste : mots ou phrases")
-}
-return choice
+
+function displayProposition (proposition) {
+    let zone = document.querySelector(".zoneProposition")
+    zone.innerText = proposition
 }
 
-function playBoucleGame(propositionList) {
-    let score = 0
-    for(let i = 0; i < propositionList.length; i++) {
-        let userWord = prompt("Entrez le mot : " + propositionList[i])
-        if (userWord === propositionList[i]){
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je vais de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
+
+function validerNom (nom) {
+    if (nom.length >= 2) {
+        return true 
+    } 
+    return false
+}
+
+function validerEmail(email) {
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+    if (emailRegExp.test(email)) {
+        return true 
+    }
+    return false 
+}
+
+function playGame() {
+    let i         = 0
+    let score     = 0
+    let totalWord = 0
+    let propositionList = listWords
+    let btnClick = document.getElementById("btnValiderMot")
+    let inputWrite = document.getElementById("inputEcriture")
+    displayProposition(propositionList[i])
+    btnClick.addEventListener("click", () => {
+        console.log(inputWrite.value)
+        if (inputWrite.value === propositionList[i]) {
             score++
         }
+        i++
+        displayResult(score, i)
+        inputWrite.value = ""
+        if(propositionList[i] === undefined) {
+            displayProposition("Le jeu est fini")
+            btnClick.disabled = true
+        } else {
+            displayProposition(propositionList[i])
+        }
+
+    })
+
+    let changeGame = document.querySelectorAll(".optionSource input")
+    for (let index = 0; index < changeGame.length; index++) {
+        changeGame[index].addEventListener("change", (event) => {
+            console.log(event.target.value)
+            if (event.target.value === "1"){
+                propositionList = listWords
+            } else {
+                propositionList = sentence
+            }
+            displayProposition(propositionList[i])
+        } )
     }
-    return score
+
+    let form = document.querySelector('form')
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+    
+
+    let nom   = document.getElementById("nom").value;
+    let email = document.getElementById("email").value
+
+    if (validerEmail(email) && validerNom(nom)) {
+        let scoreEmail = `${score} / ${i}`
+        afficherEmail(nom, email, scoreEmail)
+    } else {
+        console.log('Erreur')
+    }
+
+
+})
+
+    displayResult(score, i)
 }
+
 
 
 
